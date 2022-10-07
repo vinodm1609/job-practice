c92500666e567756a6cea2f59e7faf804be9a19a
vinodm1609 <mvinod2783@gmail.com>
2022-10-07 18:46:49 +0530

    problem solve

--- sorting/selection_sorting.js
+++ sorting/selection_sorting.js
@@ -1,38 +1,38 @@
 const arr = [12, 11, 13, 5, 6];
 
-// function select(arr) {
-//   let n = arr.length;
-//   for (let i = 0; i < n - 1; i++) {
-//     let min = i;
-//     for (let j = i + 1; j < n; j++) {
-//       if (arr[min] > arr[j]) {
-//         min = j;
-//       }
-//     }
-//     if (min !== i) {
-//       [arr[i], arr[min]] = [arr[min], arr[i]];
-//     }
-//   }
-//   return arr;
-// }
+function select(arr) {
+  let n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    let min = i;
+    for (let j = i + 1; j < n; j++) {
+      if (arr[min] > arr[j]) {
+        min = j;
+      }
+    }
+    if (min !== i) {
+      [arr[i], arr[min]] = [arr[min], arr[i]];
+    }
+  }
+  return arr;
+}
 
 // console.log(select(arr));
 
-// function select(arr) {
-//   let n = arr.length;
-//   for (let i = 0; i < n - 1; i++) {
-//     let min = i;
-//     for (let j = i + 1; j < n; j++) {
-//       if (arr[min] > arr[j]) {
-//         min = j;
-//       }
-//       if (min !== i) {
-//         [arr[i], arr[min]] = [arr[min], arr[i]];
-//       }
-//     }
-//   }
-//   return arr;
-// }
+function select(arr) {
+  let n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    let min = i;
+    for (let j = i + 1; j < n; j++) {
+      if (arr[min] > arr[j]) {
+        min = j;
+      }
+      if (min !== i) {
+        [arr[i], arr[min]] = [arr[min], arr[i]];
+      }
+    }
+  }
+  return arr;
+}
 
 // console.log(select(arr));
 
